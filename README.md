0b7fb39f1a80fc7d52ec120e9fa7690eb8011b52
Mo Kargas <mo.kargas@doshii.io>
2021-04-16 17:12:14 +1000

    Add stylelint, troubleshoot overly strict styles, modify TS import sniffing

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## An ESLint [Shareable Config](http://eslint.org/docs/developer-guide/shareable-configs) for [JavaScript Standard Style](http://standardjs.com)
 
-This module is an extension and customization of [eslint-config-airbnb-base](https://github.com/feross/eslint-config-airbnb-base) by AirBnB.
+This module is an extension and customization of [eslint-config-airbnb](https://github.com/feross/eslint-config-airbnb-base) by AirBnB.
 
 We want to provide a consistent coding standard across Doshii projects, this library extends the standard styling to conform with Doshii's opinion of best practice.
 
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    project: "./tsconfig.json",
-    sourceType: "module",
+    project: './tsconfig.json',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
       modules: true
-    },
+    }
   },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
-    "plugin:jest/recommended",
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended'
   ],
   plugins: [
-    "@typescript-eslint",
-    "import",
-    "mocha",
-    "prettier",
-    "react",
-    "react-hooks",
-    "simple-import-sort",
-    "jsx-a11y",
-    "jest",
+    '@typescript-eslint',
+    'import',
+    'mocha',
+    'prettier',
+    'react',
+    'react-hooks',
+    'simple-import-sort',
+    'jsx-a11y',
+    'jest'
   ],
   env: {
     es6: true,
@@ -35,7 +35,7 @@ module.exports = {
     mocha: true,
     browser: true,
     jquery: true,
-    "jest/globals": true
+    'jest/globals': true
   },
   globals: {
     document: false,
@@ -52,36 +52,47 @@ module.exports = {
     should: true,
     Bootstrap: true
   },
-
+  overrides: [
+    {
+      files: ['**/*.module.css.d.ts'],
+      rules: {
+        'prettier/prettier': 0
+      }
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/prefer-nullish-coalescing': 0
+      }
+    }
+  ],
   rules: {
     // https://eslint.org/docs/rules/#possible-errors
-    "arrow-parens": ["error", "as-needed"],
-    "no-underscore-dangle": 0,
-    "no-console": "error",
-    "mocha/no-exclusive-tests": "error",
-    "prettier/prettier": [
-      "error",
+    'arrow-parens': ['error', 'as-needed'],
+    'no-underscore-dangle': 0,
+    'no-console': 'error',
+    'mocha/no-exclusive-tests': 'error',
+    'prettier/prettier': [
+      'error',
       {
-        trailingComma: "none",
+        trailingComma: 'none',
         semi: true,
         printWidth: 120,
         singleQuote: true,
-        arrowParens: "avoid"
+        arrowParens: 'avoid'
       }
     ],
-    "object-shorthand": ["error", "properties"],
-    "no-return-assign": ["error", "except-parens"],
-    "func-names": ["error", "never"],
-    "comma-dangle": ["error", "never"],
-    "no-unused-vars": [
-      "error",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
-    ],
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    "one-var": ["error", { initialized: "never", uninitialized: "always" }],
-    "no-param-reassign": ["error", { props: false }],
-    "max-len": [
-      "error",
+    'object-shorthand': ['error', 'properties'],
+    'no-return-assign': ['error', 'except-parens'],
+    'func-names': ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    // 'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'one-var': ['error', { initialized: 'never', uninitialized: 'always' }],
+    'no-param-reassign': ['error', { props: false }],
+    'max-len': [
+      'error',
       120,
       {
         ignoreRegExpLiterals: true,
@@ -90,10 +101,8 @@ module.exports = {
         ignoreStrings: true
       }
     ],
-    "sort-import": 0, // Off in favor of sort-imports plugin
-    "react/jsx-filename-extension": [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    'sort-import': 0, // Off in favor of sort-imports plugin
     'no-use-before-define': 0, // Off in favor of @typescript-eslint/no-use-before-define
-    '@typescript-eslint/no-use-before-define': [1],
     'no-duplicate-imports': 0, // Off in favor of @typescript-eslint/no-duplicate-imports
     'no-sequences': 0, // See https://github.com/prettier/eslint-config-prettier#other-rules-worth-mentioning
     'no-return-await': 0, // Off in favor of @typescript-eslint/return-await
@@ -101,5 +110,28 @@ module.exports = {
     'no-shadow': 0, // Off in favor of @typescript-eslint/no-shadow
     'init-declarations': 0, // Off in favor of @typescript-eslint/init-declarations
     'no-unused-vars': 0, // Off in favor of @typescript-eslint/no-unused-vars
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+
+    // TS
+    '@typescript-eslint/no-use-before-define': [1],
+    '@typescript-eslint/explicit-function-return-type': 2,
+
+    // React
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-sort-default-props': 2
+  },
+  settings: {
+    jest: {
+      version: 26
+    }
   }
 };
