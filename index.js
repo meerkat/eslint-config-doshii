29f227d3d994bef3dcfcf9ec33226211740d9ca6
Mo Kargas <mo.kargas@doshii.io>
2021-04-19 09:34:38 +1000

    Style lint configuration, minor rules

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 0,
         '@typescript-eslint/prefer-nullish-coalescing': 0
       }
+    },
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 0
+      }
     }
   ],
   rules: {
@@ -88,7 +94,21 @@ module.exports = {
     'func-names': ['error', 'never'],
     'comma-dangle': ['error', 'never'],
     // 'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+
+    // Import rules
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+
+    // https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
+    'import/prefer-default-export': 0,
     'one-var': ['error', { initialized: 'never', uninitialized: 'always' }],
     'no-param-reassign': ['error', { props: false }],
     'max-len': [
@@ -110,24 +130,29 @@ module.exports = {
     'no-shadow': 0, // Off in favor of @typescript-eslint/no-shadow
     'init-declarations': 0, // Off in favor of @typescript-eslint/init-declarations
     'no-unused-vars': 0, // Off in favor of @typescript-eslint/no-unused-vars
-    'import/extensions': [
+    'no-plusplus': 2,
+
+    // TS rules
+    '@typescript-eslint/no-use-before-define': 1,
+    '@typescript-eslint/explicit-function-return-type': [
       2,
-      'ignorePackages',
       {
-        js: 'never',
-        ts: 'never',
-        tsx: 'never'
+        allowExpressions: true
       }
     ],
 
-    // TS
-    '@typescript-eslint/no-use-before-define': [1],
-    '@typescript-eslint/explicit-function-return-type': 2,
-
-    // React
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    // React rules
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
     'react/jsx-props-no-spreading': 0,
-    'react/jsx-sort-default-props': 2
+    'react/jsx-sort-default-props': 2,
+    'react/prop-types': 2,
+    'react/require-default-props': [2, { ignoreFunctionalComponents: true }],
+    'react/jsx-first-prop-new-line': 0, // Off in favor of prettier/prettier
+    'react/jsx-fragments': 2,
+    'react/jsx-handler-names': 2,
+    'react/jsx-indent': 0, // Off in favor of prettier/prettier
+    'react/jsx-indent-props': 0, // Off in favor of prettier/prettier
+    'react/jsx-key': 2
   },
   settings: {
     jest: {
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-standard",
-    "stylelint-config-css-modules",
-    "stylelint-config-sass-guidelines",
-    "stylelint-config-prettier"
+    'stylelint-config-standard',
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-prettier'
   ],
-  plugins: [
-    "stylelint-no-unused-selectors",
-    "stylelint-order"
-  ],
+  plugins: ['stylelint-no-unused-selectors', 'stylelint-order'],
   rules: {
-    "order/properties-alphabetical-order": true,
-    "plugin/no-unused-selectors": true
+    'order/properties-alphabetical-order': true,
+    'plugin/no-unused-selectors': true
   }
 };
